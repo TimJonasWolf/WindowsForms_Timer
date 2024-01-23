ab342845b25ed4f8a65d74cc0bd4ec1445d987b1
TimJonasWolf <132747647+TimJonasWolf@users.noreply.github.com>
2024-01-23 22:48:19 +0100

    Add files via upload

---
+++ WindowsForms_Timer/WindowsForms_Timer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsForms_Timer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(169, 39);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(409, 130);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Timer";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(73, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(270, 69);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "00:00:00";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.LimeGreen;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(124, 271);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(160, 42);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Red;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(313, 271);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(160, 42);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Stop";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.LightSalmon;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(513, 271);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(160, 42);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Reset";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// timer
+			// 
+			this.timer.Tag = "1000";
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LemonChiffon;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Timer timer;
+	}
+}
+
---
+++ WindowsForms_Timer/WindowsForms_Timer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Timer
+{
+	public partial class Form1 : Form
+	{
+		private TimeSpan timeL;
+		Boolean weiter = true;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			timeL = timeL.Add(TimeSpan.FromSeconds(1)); // mit Add addiert und zb mit Subtract geht es ins minus
+
+			label1.Text = timeL.ToString(@"hh\:mm\:ss"); // @ ist ein zeichen der dokumentation ka für was aber zeigt einfach strings
+			if (timeL.TotalSeconds >= 30) 
+			{
+				timer.Stop();
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timeL = new TimeSpan(Convert.ToInt32(timeL.TotalHours), Convert.ToInt32(timeL.TotalMinutes), Convert.ToInt32(timeL.TotalSeconds));
+			label1.Text = timeL.ToString(@"hh\:mm\:ss");
+			timer.Start();
+
+			if (timeL.TotalSeconds == 30) 
+			{
+				timeL = new TimeSpan(0, 0, 0);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer.Stop();
+			label1.Text = timeL.ToString(@"hh\:mm\:ss");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			timer.Stop();
+
+			timeL = new TimeSpan(0, 0, 0);
+			label1.Text = timeL.ToString(@"hh\:mm\:ss");
+		}
+
+
+	}
+}
